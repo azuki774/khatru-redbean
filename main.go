f740de8bf962c4e265b590d020d475d417a95e16
azuki774 <azuki774s@gmail.com>
2025-12-07 02:41:16 +0000

    minimal http server

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/azuki774/khatru-redbean/cmd"
+import (
+	"github.com/azuki774/khatru-redbean/cmd"
+	logger "github.com/azuki774/khatru-redbean/internal/logger"
+)
 
 func main() {
+	glogger := logger.Load()
+	defer glogger.Sync() // 必要
 	cmd.Execute()
 }
